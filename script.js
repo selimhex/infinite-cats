39fb8a617c30b4e4a71ac9abb1d4e72432b0cf52
Szm Hxz <selimhex@gmail.com>
2020-05-25 01:48:55 +0200

    debugging made better && code cleanup

--- script.js
+++ script.js
@@ -6,78 +6,93 @@ var url = 'https://aws.random.cat/meow';
 //let status = { };
 let i = 1;
 let fetchloop = 1;
-let fetchloopDone = true;
-let working = false;
+var fetchloopDone = true;
+var working = false;
 let loadedimgcount = 0;
 let debugMode = false;
-if (debugMode) {body.classList.add("debugMode");}
+let debuglite = true;
+let loggerclicker;
+let debug = function(){};
 
 let checkIfWorking = function() {
-let wasWorking = working;
-logger();
-   if (loadedimgcount < i-3 || !fetchloopDone) {working = true} else {working = false;}
-   if(wasWorking != working){
-     //console.log("working:", working, "fetchloopDone", fetchloopDone, loadedimgcount + "/" + (i-1));
-   }
-if(!working) {shouldIinfiniteScroll();}
-}
+  let wasWorking = working;
+  if (loadedimgcount < i-3 || !fetchloopDone) {working = true} else {working = false;}
+  if(/* wasWorking != working || */
+    loadedimgcount == i-1){
+      logger();
+    }
+    if(!debuglite){logger()};
+    if(!working) {shouldIinfiniteScroll();}
+  }
 
-let logger = function() {
-  //console.log(`${loadedimgcount} / ${i-1} ::: ${fetchloop-1} / 10`);
-}
-let logger2 = function(msg) {
+let icons = {fetchloopDone : {true: "✅", false: "🔄" },
+            loading: function(){return this.idle[(loadedimgcount == (i-1))];},
+            idle : {true: "✅", false: "🔄" },
+            working : {true: "💪", false: "😎"}
+            }
+let icon = function(e){return icons[e][window[e]];}
+
+let logger = function(msg, standardOutput) {
+  let output;
   if (debugMode) {
     if (!msg){ msg = ""}else{ msg += " ";}
-    console.log(`${msg}working: ${working} ::: loaded: ${loadedimgcount} / ${i-1} ::: fetchloop ${fetchloop-1} / 10 ${fetchloopDone}`);
+    //msg += `standardOutput: ${standardOutput} `;
+    if (standardOutput != false) {
+      output = `working: ${icon("working")} ::: loaded: ${loadedimgcount} / ${i-1} ${icons.loading()} ::: fetchloop ${fetchloop-1} / 10 ${icon("fetchloopDone")}`;
+    } else {output = ""}
+    console.log(`${msg}${output}`);
   }
 }
+
+debug = function(b){
+  if (b)
+    { debugMode = true; body.classList.add("debugMode");  loggerclicker = setInterval(logger, 1000, "🕰");
+  } else { debugMode = false; body.classList.remove("debugMode"); clearInterval(loggerclicker); }
+}
+debug(debugMode);
 
 let initCatter = function(catcount) {
   fetchloop = 1;
   for (let forlooper = 0; forlooper < catcount; forlooper++) {
-      let prev_fetchloopDone = fetchloopDone;
-      fetchloopDone = false;
-      if (prev_fetchloopDone != fetchloopDone) {
-        logger2();
-      }
-      fetch(url)
-        .then(res => res.json())
-        .then((out) => {
-          let img = document.createElement("img"); // thumbnail
-          let a = document.createElement("a"); // thumbnail_container
-          let img2 = document.createElement("img"); // lightbox
-          let a2 = document.createElement("a"); // lightbox container named #img1, #img2 etc.
+    let prev_fetchloopDone = fetchloopDone;
+    fetchloopDone = false;
+    if (prev_fetchloopDone != fetchloopDone) { logger(); }
+    fetch(url)
+      .then(res => res.json())
+      .then((out) => {
+        let img = document.createElement("img"); // thumbnail
+        let a = document.createElement("a"); // thumbnail_container
+        let img2 = document.createElement("img"); // lightbox
+        let a2 = document.createElement("a"); // lightbox container named #img1, #img2 etc.
 
-          img.src = out.file;
-          img.classList.add("thumbnail");
-          img.loading = "lazy";
-          img.onload = function() {loadedimgcount++; /* logger();*/ checkIfWorking();}
-          a.classList.add("thumbnail_container");
-          a.href = "#img" + i;
-          let probability = chance.natural({ min: 1, max: 100 })
+        img.src = out.file;
+        img.classList.add("thumbnail");
+        img.loading = "lazy";
+        img.onload = function() {loadedimgcount++; checkIfWorking();}
+        a.classList.add("thumbnail_container");
+        a.href = "#img" + i;
+        let probability = chance.natural({ min: 1, max: 100 }) // https://github.com/chancejs/chancejs
 
-          if (probability < 30) {img.classList.add("max30");}
-          if (probability > 60) {img.classList.add("max60");}
-          if (probability > 75) {img.classList.add("max85");}
+        if (probability < 30) {img.classList.add("max30");}
+        if (probability > 60) {img.classList.add("max60");}
+        if (probability > 75) {img.classList.add("max85");}
 
-          img2.src = out.file;
-          img2.loading = "lazy";
-          a2.classList.add("lightbox");
-          a2.href = "#_";
-          a2.id = "img" + i;
+        img2.src = out.file;
+        img2.loading = "lazy";
+        a2.classList.add("lightbox");
+        a2.href = "#_";
+        a2.id = "img" + i;
 
-          a.appendChild(img);
-          a2.appendChild(img2);
-          main.appendChild(a);
-          main.appendChild(a2);
-        }).catch(err => console.error(err))
-        .finally(() => {fetchloop++; i++;logger();
-                        if (fetchloop-1 == catcount) {
-                        fetchloopDone = true;
-                        logger2();
-                        checkIfWorking();
-                        //setTimeout(function() {working = false; /*shouldIinfiniteScroll();*/}, 1000);
-                        //working = false;
+        a.appendChild(img);
+        a2.appendChild(img2);
+        main.appendChild(a);
+        main.appendChild(a2);
+      }).catch(err => console.error(err))
+      .finally(() => {fetchloop++; i++;
+                      if (fetchloop-1 == catcount) {
+                      fetchloopDone = true;
+                      logger();
+                      checkIfWorking(); //working = false;
 
                       } else {fetchloopDone = false;}
       })
@@ -86,9 +101,9 @@ let initCatter = function(catcount) {
 }
 initCatter(10);
 
+
 //let autoCat = setInterval(newCat, 10000);
-//let loadedImgCounter = setInterval(function(){console.log("loaded imgs:",loadedimgcount)}, 1000);
-let logger2clicker = setInterval(logger2, 1000, "🕰");
+//let loggerclicker = setInterval(logger, 1000, "🕰");
 
 document.addEventListener("scroll", function(event) {
   shouldIinfiniteScroll();
@@ -96,7 +111,6 @@ document.addEventListener("scroll", function(event) {
 
 
 var shouldIinfiniteScroll = function() {
-  //checkIfWorking();
   if (!working) {
     var lastImg = document.querySelector(".thumbnail_container:nth-last-child(2) img");
     if (lastImg != undefined) {
@@ -104,20 +118,13 @@ var shouldIinfiniteScroll = function() {
       var pageOffset = window.pageYOffset + window.innerHeight;
       if (pageOffset > lastImgOffset - (window.innerHeight * .5)) {
         working = true;
-        //console.log("NEXTING", loadedimgcount +"/"+ (i-1));
-        logger2("🔄")
-        lastImg.style["padding-bottom"] = "30vh";
+        //lastImg.style["padding-bottom"] = "30vh";
+        logger("⏩⏩⏩ NEXTING ⏩⏩⏩", false);
         initCatter(10);
         shouldIinfiniteScroll();
-        setTimeout(function() {
-          lastImg.style["padding-bottom"] = "0px";
-          //console.log("setting padding bottom to 0");
-        }, 0);
-
       }
     }
-  } else {
+  } else { // says it's working + check if really true
     checkIfWorking();
-    logger2();
   }
 };
