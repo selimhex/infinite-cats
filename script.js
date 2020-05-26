35d3cf4d1787746708a51974b203c075ded8964a
Szm Hxz <selimhex@gmail.com>
2020-05-26 13:24:40 +0200

    added key controls to lightbox (<,>,esc,c) + alternative lightbox view

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 'use strict';
+const $ = (a) => {return document.querySelector(a)};
+const qA = (a) => {return document.querySelectorAll(a)};
 let main = document.querySelector("main");
+let mainThumbs = $("main .thumbs");
+let mainLightboxes = $("main .lightboxes");
+
 let body = document.querySelector("body");
 
 var url = 'https://aws.random.cat/meow';
@@ -9,7 +14,7 @@ let fetchloop = 1;
 var fetchloopDone = true;
 var working = false;
 let loadedimgcount = 0;
-let debugMode = false;
+let debugMode = true;
 let debuglite = true;
 let loggerclicker;
 let debug = function(){};
@@ -70,6 +75,7 @@ let initCatter = function(catcount, lazyload) {
         img.onload = function() {loadedimgcount++; checkIfWorking();}
         a.classList.add("thumbnail_container");
         a.href = "#img" + i;
+        a.id = "th" + i;
         let probability = chance.natural({ min: 1, max: 100 }) // https://github.com/chancejs/chancejs
 
         if (probability < 30) {img.classList.add("max30");}
@@ -78,7 +84,7 @@ let initCatter = function(catcount, lazyload) {
 
         img2.src = out.file;
         a2.classList.add("lightbox");
-        a2.href = "#_";
+        a2.href = "#th" +i;
         a2.id = "img" + i;
 
         if (lazyload != false) {
@@ -87,8 +93,8 @@ let initCatter = function(catcount, lazyload) {
 
         a.appendChild(img);
         a2.appendChild(img2);
-        main.appendChild(a);
-        main.appendChild(a2);
+        mainThumbs.appendChild(a);
+        mainLightboxes.appendChild(a2);
       }).catch(err => console.error(err))
       .finally(() => {fetchloop++; i++;
                       if (fetchloop-1 == catcount) {
@@ -130,3 +136,60 @@ var shouldIinfiniteScroll = function() {
     checkIfWorking();
   }
 };
+
+var key, idnum, targetId, clearcheck;
+let repeatcheck;
+
+
+document.addEventListener('keydown', event => {
+//console.log(event);
+key = event.key;
+  if(event.target.classList.contains("lightbox")) {
+
+    targetId = event.target.id;
+    idnum = event.target.id.slice(3);
+    console.log(idnum);
+    //console.log(key);
+    if (key === "ArrowRight") {
+      idnum++;
+//#################################################### this part could be better
+      if (!$("#img" + idnum)) { //if the next lightbox img is not generated yet
+        //clearcheck = setInterval(repeatcheck,500,idnum);
+        checkIfWorking();
+        if(!fetchloopDone || loadedimgcount < (i-7)){
+
+        } else {initCatter(10);clearcheck = setTimeout(repeatcheck,100,idnum);} // setInterval led to chaos. trying to control it with setTimeout.
+
+        repeatcheck = function(idnum) {
+            //console.log("ping");
+            if ($("#img" + idnum)) {
+                clearTimeout(clearcheck);
+                console.log("clicking");
+                $("#th" +idnum).click();
+            } else {console.log("ping");clearcheck = setTimeout(repeatcheck,100,idnum);}
+        }
+//#################################################### this part could be better
+      } else {
+        $("#th" +idnum).click();
+      }
+
+    } else if (key === "ArrowLeft") {
+      idnum--;
+        if ($("#img" + idnum)) {
+          $("#th" +idnum).click();
+        }
+    } else if (key === "Escape") {
+        $("#" +targetId).click();
+    } else if (key === "c" || key === "C") {
+      mainLightboxes.classList.toggle("fit2screen");
+
+    }
+  //console.log(idnum);
+  }
+
+
+
+
+
+
+});
